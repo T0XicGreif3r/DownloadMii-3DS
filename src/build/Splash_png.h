9dce9de874271d31cf1c8c51d6f64ea9753a27bc
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2015-08-18 14:31:34 +0200

    Remove unused files.

--- src/build/Splash_png.h
+++
@@ -1,3 +0,0 @@
-extern const u8 Splash_png_end[];
-extern const u8 Splash_png[];
-extern const u32 Splash_png_size;
--- src/include2/error.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <3ds.h>
-#include <vector>
-
-
-//ToDo
-char* getErrorMsg(Result error);
