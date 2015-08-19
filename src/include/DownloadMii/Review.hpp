9e995d3e9b730db2851f10962e09d3608d0d44e8
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2015-08-19 10:05:25 +0200

    Update copyright to reflect recent company changes.

--- src/include/DownloadMii/Review.hpp
+++ src/include/DownloadMii/Review.hpp
@@ -5,7 +5,7 @@
 //   | |__| | (_) \ V  V /| | | | | (_) | (_| | (_| | |  | | | |
 //   |_____/ \___/ \_/\_/ |_| |_|_|\___/ \__,_|\__,_|_|  |_|_|_|
 //                                                              
-//   ©2014-2015 Filiph Sandström & Contributors
+//   ©2014-2015 filfat Studios AB
 //   Please see the LICENSE for more info!
 //
 //	File name: Review.hpp
